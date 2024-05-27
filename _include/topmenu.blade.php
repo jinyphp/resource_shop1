7209981ac4932e18aa558f7cc2f2530597b15b2f
hojinlee <infohojin@gmail.com>
2024-05-27 20:38:36 +0900

    레이아웃 분리

---
+++ _include/topmenu.blade.php
@@ -0,0 +1,258 @@
+<!-- Navbar nav -->
+<div class="col-lg-9 d-lg-flex pt-3 pt-lg-0 ps-lg-0">
+  <ul class="navbar-nav position-relative">
+    <li class="nav-item dropdown me-lg-n1 me-xl-0">
+      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" aria-expanded="false">Home</a>
+      <ul class="dropdown-menu">
+        <li class="hover-effect-opacity px-2 mx-n2">
+          <a class="dropdown-item d-block mb-0" href="home-electronics">
+            <span class="fw-medium">Electronics Store</span>
+            <span class="d-block fs-xs text-body-secondary">Megamenu + Hero slider</span>
+            <div class="d-none d-lg-block hover-effect-target position-absolute top-0 start-100 bg-body border border-light-subtle rounded rounded-start-0 transition-none invisible opacity-0 pt-2 px-2 ms-n2" style="width: 212px; height: calc(100% + 2px); margin-top: -1px">
+              <img class="position-relative z-2 d-none-dark" src="assets/img/mega-menu/demo-preview/electronics-light.jpg" alt="Electronics Store">
+              <img class="position-relative z-2 d-none d-block-dark" src="assets/img/mega-menu/demo-preview/electronics-dark.jpg" alt="Electronics Store">
+              <span class="position-absolute top-0 start-0 w-100 h-100 rounded rounded-start-0 d-none-dark" style="box-shadow: .875rem .5rem 2rem -.5rem #676f7b; opacity: .1"></span>
+              <span class="position-absolute top-0 start-0 w-100 h-100 rounded rounded-start-0 d-none d-block-dark" style="box-shadow: .875rem .5rem 1.875rem -.5rem #080b12; opacity: .25"></span>
+            </div>
+          </a>
+        </li>
+        <li class="hover-effect-opacity px-2 mx-n2">
+          <a class="dropdown-item d-block mb-0" href="home-fashion-v1">
+            <span class="fw-medium">Fashion Store v.1</span>
+            <span class="d-block fs-xs text-body-secondary">Hero promo slider</span>
+            <div class="d-none d-lg-block hover-effect-target position-absolute top-0 start-100 bg-body border border-light-subtle rounded rounded-start-0 transition-none invisible opacity-0 pt-2 px-2 ms-n2" style="width: 212px; height: calc(100% + 2px); margin-top: -1px">
+              <img class="position-relative z-2 d-none-dark" src="assets/img/mega-menu/demo-preview/fashion-1-light.jpg" alt="Fashion Store v.1">
+              <img class="position-relative z-2 d-none d-block-dark" src="assets/img/mega-menu/demo-preview/fashion-1-dark.jpg" alt="Fashion Store v.1">
+              <span class="position-absolute top-0 start-0 w-100 h-100 rounded rounded-start-0 d-none-dark" style="box-shadow: .875rem .5rem 2rem -.5rem #676f7b; opacity: .1"></span>
+              <span class="position-absolute top-0 start-0 w-100 h-100 rounded rounded-start-0 d-none d-block-dark" style="box-shadow: .875rem .5rem 1.875rem -.5rem #080b12; opacity: .25"></span>
+            </div>
+          </a>
+        </li>
+        <li class="hover-effect-opacity px-2 mx-n2">
+          <a class="dropdown-item d-block mb-0" href="home-fashion-v2">
+            <span class="fw-medium">Fashion Store v.2</span>
+            <span class="d-block fs-xs text-body-secondary">Hero banner with hotspots</span>
+            <div class="d-none d-lg-block hover-effect-target position-absolute top-0 start-100 bg-body border border-light-subtle rounded rounded-start-0 transition-none invisible opacity-0 pt-2 px-2 ms-n2" style="width: 212px; height: calc(100% + 2px); margin-top: -1px">
+              <img class="position-relative z-2 d-none-dark" src="assets/img/mega-menu/demo-preview/fashion-2-light.jpg" alt="Fashion Store v.2">
+              <img class="position-relative z-2 d-none d-block-dark" src="assets/img/mega-menu/demo-preview/fashion-2-dark.jpg" alt="Fashion Store v.2">
+              <span class="position-absolute top-0 start-0 w-100 h-100 rounded rounded-start-0 d-none-dark" style="box-shadow: .875rem .5rem 2rem -.5rem #676f7b; opacity: .1"></span>
+              <span class="position-absolute top-0 start-0 w-100 h-100 rounded rounded-start-0 d-none d-block-dark" style="box-shadow: .875rem .5rem 1.875rem -.5rem #080b12; opacity: .25"></span>
+            </div>
+          </a>
+        </li>
+        <li class="hover-effect-opacity px-2 mx-n2">
+          <a class="dropdown-item d-block mb-0" href="home-furniture">
+            <span class="fw-medium">Furniture Store</span>
+            <span class="d-block fs-xs text-body-secondary">Fancy product carousel</span>
+            <div class="d-none d-lg-block hover-effect-target position-absolute top-0 start-100 bg-body border border-light-subtle rounded rounded-start-0 transition-none invisible opacity-0 pt-2 px-2 ms-n2" style="width: 212px; height: calc(100% + 2px); margin-top: -1px">
+              <img class="position-relative z-2 d-none-dark" src="assets/img/mega-menu/demo-preview/furniture-light.jpg" alt="Furniture Store">
+              <img class="position-relative z-2 d-none d-block-dark" src="assets/img/mega-menu/demo-preview/furniture-dark.jpg" alt="Furniture Store">
+              <span class="position-absolute top-0 start-0 w-100 h-100 rounded rounded-start-0 d-none-dark" style="box-shadow: .875rem .5rem 2rem -.5rem #676f7b; opacity: .1"></span>
+              <span class="position-absolute top-0 start-0 w-100 h-100 rounded rounded-start-0 d-none d-block-dark" style="box-shadow: .875rem .5rem 1.875rem -.5rem #080b12; opacity: .25"></span>
+            </div>
+          </a>
+        </li>
+        <li class="hover-effect-opacity px-2 mx-n2">
+          <a class="dropdown-item d-block mb-0" href="home-grocery">
+            <span class="fw-medium">Grocery Store</span>
+            <span class="d-block fs-xs text-body-secondary">Hero slider + Category cards</span>
+            <div class="d-none d-lg-block hover-effect-target position-absolute top-0 start-100 bg-body border border-light-subtle rounded rounded-start-0 transition-none invisible opacity-0 pt-2 px-2 ms-n2" style="width: 212px; height: calc(100% + 2px); margin-top: -1px">
+              <img class="position-relative z-2 d-none-dark" src="assets/img/mega-menu/demo-preview/grocery-light.jpg" alt="Grocery Store">
+              <img class="position-relative z-2 d-none d-block-dark" src="assets/img/mega-menu/demo-preview/grocery-dark.jpg" alt="Grocery Store">
+              <span class="position-absolute top-0 start-0 w-100 h-100 rounded rounded-start-0 d-none-dark" style="box-shadow: .875rem .5rem 2rem -.5rem #676f7b; opacity: .1"></span>
+              <span class="position-absolute top-0 start-0 w-100 h-100 rounded rounded-start-0 d-none d-block-dark" style="box-shadow: .875rem .5rem 1.875rem -.5rem #080b12; opacity: .25"></span>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </li>
+    <li class="nav-item dropdown position-static me-lg-n1 me-xl-0">
+      <a class="nav-link dropdown-toggle active" aria-current="page" href="#" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" aria-expanded="false">Shop</a>
+      <div class="dropdown-menu rounded-4 p-4">
+        <div class="d-flex flex-column flex-lg-row gap-4">
+          <div style="min-width: 190px">
+            <div class="h6 mb-2">Electronics Store</div>
+            <ul class="nav flex-column gap-2 mt-0">
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="shop-categories-electronics">Categories Page</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="shop-catalog-electronics">Catalog with Side Filters</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="shop-product-general-electronics">Product General Info</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="shop-product-details-electronics">Product Details</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="shop-product-reviews-electronics">Product Reviews</a>
+              </li>
+            </ul>
+            <div class="h6 pt-4 mb-2">Fashion Store</div>
+            <ul class="nav flex-column gap-2 mt-0">
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="shop-catalog-fashion">Catalog with Side Filters</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="shop-product-fashion">Product Page</a>
+              </li>
+            </ul>
+            <div class="h6 pt-4 mb-2">Furniture Store</div>
+            <ul class="nav flex-column gap-2 mt-0">
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="shop-catalog-furniture">Catalog with Top Filters</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="shop-product-furniture">Product Page</a>
+              </li>
+            </ul>
+          </div>
+          <div style="min-width: 190px">
+            <div class="h6 mb-2">Grocery Store</div>
+            <ul class="nav flex-column gap-2 mt-0">
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="shop-catalog-grocery">Catalog with Side Filters</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="shop-product-grocery">Product Page</a>
+              </li>
+            </ul>
+            <div class="h6 pt-4 mb-2">Checkout v.1</div>
+            <ul class="nav flex-column gap-2 mt-0">
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="checkout-v1-cart">Shopping Cart</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="checkout-v1-delivery-1">Delivery Info (Step 1)</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="checkout-v1-delivery-2">Delivery Info (Step 2)</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="checkout-v1-shipping">Shipping Address</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="checkout-v1-payment">Payment</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="checkout-v1-thankyou">Thank You Page</a>
+              </li>
+            </ul>
+          </div>
+          <div style="min-width: 190px">
+            <div class="h6 mb-2">Checkout v.2</div>
+            <ul class="nav flex-column gap-2 mt-0">
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="checkout-v2-cart">Shopping Cart</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="checkout-v2-delivery">Delivery Info</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="checkout-v2-pickup">Pickup from Store</a>
+              </li>
+              <li class="d-flex w-100 pt-1">
+                <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="checkout-v2-thankyou">Thank You Page</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </li>
+    <li class="nav-item dropdown me-lg-n1 me-xl-0">
+      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" data-bs-auto-close="outside" aria-expanded="false">Account</a>
+      <ul class="dropdown-menu">
+        <li class="dropend">
+          <a class="dropdown-item dropdown-toggle" href="#!" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" aria-expanded="false">Auth Pages</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="account-signin">Sign In</a></li>
+            <li><a class="dropdown-item" href="account-signup">Sign Up</a></li>
+            <li><a class="dropdown-item" href="account-password-recovery">Password Recovery</a></li>
+          </ul>
+        </li>
+        <li><a class="dropdown-item" href="account-orders">Orders History</a></li>
+        <li><a class="dropdown-item" href="account-wishlist">Wishlist</a></li>
+        <li><a class="dropdown-item" href="account-payment">Payment Methods</a></li>
+        <li><a class="dropdown-item" href="account-reviews">My Reviews</a></li>
+        <li><a class="dropdown-item" href="account-info">Personal Info</a></li>
+        <li><a class="dropdown-item" href="account-addresses">Addresses</a></li>
+        <li><a class="dropdown-item" href="account-notifications">Notifications</a></li>
+      </ul>
+    </li>
+    <li class="nav-item dropdown me-lg-n1 me-xl-0">
+      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" data-bs-auto-close="outside" aria-expanded="false">Pages</a>
+      <ul class="dropdown-menu">
+        <li class="dropend">
+          <a class="dropdown-item dropdown-toggle" href="#!" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" aria-expanded="false">About</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="about-v1">About v.1</a></li>
+            <li><a class="dropdown-item" href="about-v2">About v.2</a></li>
+          </ul>
+        </li>
+        <li class="dropend">
+          <a class="dropdown-item dropdown-toggle" href="#!" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" aria-expanded="false">Blog</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="blog-grid-v1">Grid View v.1</a></li>
+            <li><a class="dropdown-item" href="blog-grid-v2">Grid View v.2</a></li>
+            <li><a class="dropdown-item" href="blog-list">List View</a></li>
+            <li><a class="dropdown-item" href="blog-single-v1">Single Post v.1</a></li>
+            <li><a class="dropdown-item" href="blog-single-v2">Single Post v.2</a></li>
+          </ul>
+        </li>
+        <li class="dropend">
+          <a class="dropdown-item dropdown-toggle" href="#!" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" aria-expanded="false">Contact</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="contact-v1">Contact v.1</a></li>
+            <li><a class="dropdown-item" href="contact-v2">Contact v.2</a></li>
+            <li><a class="dropdown-item" href="contact-v3">Contact v.3</a></li>
+          </ul>
+        </li>
+        <li class="dropend">
+          <a class="dropdown-item dropdown-toggle" href="#!" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" aria-expanded="false">Help Center</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="help-topics-v1">Help Topics v.1</a></li>
+            <li><a class="dropdown-item" href="help-topics-v2">Help Topics v.2</a></li>
+            <li><a class="dropdown-item" href="help-single-article-v1">Help Single Article v.1</a></li>
+            <li><a class="dropdown-item" href="help-single-article-v2">Help Single Article v.2</a></li>
+          </ul>
+        </li>
+        <li class="dropend">
+          <a class="dropdown-item dropdown-toggle" href="#!" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" aria-expanded="false">404 Error</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="404-electronics">404 Electronics</a></li>
+            <li><a class="dropdown-item" href="404-fashion">404 Fashion</a></li>
+            <li><a class="dropdown-item" href="404-furniture">404 Furniture</a></li>
+            <li><a class="dropdown-item" href="404-grocery">404 Grocery</a></li>
+          </ul>
+        </li>
+        <li><a class="dropdown-item" href="terms-and-conditions">Terms &amp; Conditions</a></li>
+      </ul>
+    </li>
+    <li class="nav-item me-lg-n2 me-xl-0">
+      <a class="nav-link" href="docs/installation">Docs</a>
+    </li>
+    <li class="nav-item me-lg-n2 me-xl-0">
+      <a class="nav-link" href="docs/typography">Components</a>
+    </li>
+  </ul>
+  <hr class="d-lg-none my-3">
+  <ul class="navbar-nav ms-auto">
+    <li class="nav-item dropdown me-lg-n2 me-xl-n1">
+      <a class="nav-link dropdown-toggle fs-sm px-3" href="#!" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" aria-expanded="false">Eng</a>
+      <ul class="dropdown-menu fs-sm" style="--cz-dropdown-min-width: 7.5rem; --cz-dropdown-spacer: .25rem">
+        <li><a class="dropdown-item" href="#!">Français</a></li>
+        <li><a class="dropdown-item" href="#!">Deutsch</a></li>
+        <li><a class="dropdown-item" href="#!">Italiano</a></li>
+      </ul>
+    </li>
+    <li class="nav-item dropdown me-lg-n1">
+      <a class="nav-link dropdown-toggle fs-sm px-3" href="#!" role="button" data-bs-toggle="dropdown" data-bs-trigger="hover" aria-expanded="false">USD ($)</a>
+      <ul class="dropdown-menu dropdown-menu-end fs-sm" style="--cz-dropdown-min-width: 7rem; --cz-dropdown-spacer: .25rem">
+        <li><a class="dropdown-item" href="#!">€ EUR</a></li>
+        <li><a class="dropdown-item" href="#!">£ UKP</a></li>
+        <li><a class="dropdown-item" href="#!">¥ JPY</a></li>
+      </ul>
+    </li>
+  </ul>
+</div>
